6212bb14c10f6084ac8458a4e06eee6ea0eaf238
rmakea <make73260@gmail.com>
2024-12-05 16:43:17 -0600

    CBDF-12

---
+++ src/app/saludo/saludo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saludo',
+  imports: [],
+  templateUrl: './saludo.component.html',
+  styleUrl: './saludo.component.css'
+})
+export class SaludoComponent {
+
+}
